b466bdea38760c580a9471d8a22355b17c2fe1e7
ThiraTheNerd <thiragithinji@gmail.com>
2021-02-28 17:40:14 +0300

    add the functions to change about us apperance when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
+  $('#item1').click(function(){
+    $('#item1').hide();
+    $('#design').show();
+  });
+  $('#design').click(function(){
+    $('#design').hide();
+    $('#item1').show();
+  })
+});
+$(document).ready(function(){
+  $('#item2').click(function(){
+    $('#item2').hide();
+    $('#development').show();
+  });
+  $('#development').click(function(){
+    $('#development').hide();
+    $('#item2').show();
+  })
+});
+$(document).ready(function(){
+  $('#item3').click(function(){
+    $('#item3').hide();
+    $('#project-mgnt').show();
+  });
+  $('#project-mgnt').click(function(){
+    $('#project-mgnt').hide();
+    $('#item3').show();
+  })
+});
+
+
+
+
+$(document).ready(function(){
   $('#work1').mouseover(function(){
     $('#job1').show();
   }).mouseout(function(){
@@ -31,5 +65,26 @@ $(document).ready(function(){
     $('#job5').show();
   }).mouseout(function(){
     $('#job5').hide()
+  });
+});
+$(document).ready(function(){
+  $('#work6').mouseover(function(){
+    $('#job6').show();
+  }).mouseout(function(){
+    $('#job6').hide()
+  });
+});
+$(document).ready(function(){
+  $('#work7').mouseover(function(){
+    $('#job7').show();
+  }).mouseout(function(){
+    $('#job7').hide()
+  });
+});
+$(document).ready(function(){
+  $('#work8').mouseover(function(){
+    $('#job8').show();
+  }).mouseout(function(){
+    $('#job8').hide()
   });
 });
